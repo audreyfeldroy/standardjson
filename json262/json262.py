49c00236569d48f651bd8f2226907d5c784cbe77
Audrey Roy <audreyr@gmail.com>
2014-05-18 22:52:35 -0700

    Bring in encoder from webhooks.

--- json262/json262.py
+++ json262/json262.py
@@ -1,1 +1,42 @@
 # -*- coding: utf-8 -*-
+
+"""
+Serialize data to/from JSON
+Inspired by https://github.com/django/django/blob/master/django/core/serializers/json.py
+"""
+
+# Avoid shadowing the standard library json module
+from __future__ import absolute_import
+from __future__ import unicode_literals
+
+import datetime
+import decimal
+import json
+
+
+class JSON262Encoder(json.JSONEncoder):
+    """
+    JSON encoder aiming to be fully compliant with ECMA-262.
+    """
+    def default(self, o):
+        # See "Date Time String Format" in the ECMA-262 specification.
+        if isinstance(o, datetime.datetime):
+            r = o.isoformat()
+            if o.microsecond:
+                r = r[:23] + r[26:]
+            if r.endswith('+00:00'):
+                r = r[:-6] + 'Z'
+            return r
+        elif isinstance(o, datetime.date):
+            return o.isoformat()
+        elif isinstance(o, datetime.time):
+            r = o.isoformat()
+            if o.microsecond:
+                r = r[:12]
+            if r.endswith('+00:00'):
+                r = r[:-6] + 'Z'
+            return r
+        elif isinstance(o, decimal.Decimal):
+            return str(o)
+        else:
+            return super(JSON262Encoder, self).default(o)
