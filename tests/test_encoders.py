1bf0bfd681ab25dda099a4e6e00c8d0abb4ab8e0
Audrey Roy <audreyr@gmail.com>
2014-05-20 09:45:45 -0700

    TDD for date and time functions that don't exist yet. Tests should fail.

--- tests/test_encoders.py
+++ tests/test_encoders.py
@@ -17,6 +17,16 @@ import json
 
 
 def test_encode_datetime():
-    """ JSON262Encoder should work with datetimes. """
+    """ encode_datetime() should return a ECMA-262 compliant datetime string. """
     val = datetime.datetime(2006, 11, 21, 16, 30)
     assert encode_datetime(val) == '2006-11-21T16:30:00'
+
+def test_encode_date():
+    """ encode_date() should return a ECMA-262 compliant date string. """
+    val = datetime.date(2006, 11, 21)
+    assert encode_date(val) == '2006-11-21'
+
+def test_encode_time():
+    """ encode_time() should return a ECMA-262 compliant time string. """
+    val = datetime.time(16, 30)
+    assert encode_time(val) == '16:30:00'
