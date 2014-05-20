9960a793abe4e11fcd7a18450f6a6fcb12a92be3
Audrey Roy <audreyr@gmail.com>
2014-05-20 10:29:58 -0700

    Docstrings for encoder functions.

--- json262/encoders.py
+++ json262/encoders.py
@@ -1,6 +1,8 @@
 # -*- coding: utf-8 -*-
 
 def encode_datetime(o):
+    """ Encodes a Python datetime.datetime object as an ECMA-262 compliant
+    datetime string."""
     r = o.isoformat()
     if o.microsecond:
         r = r[:23] + r[26:]
@@ -9,9 +11,13 @@ def encode_datetime(o):
     return r
 
 def encode_date(o):
+    """ Encodes a Python datetime.date object as an ECMA-262 compliant
+    date string."""
     return o.isoformat()
 
 def encode_time(o):
+    """ Encodes a Python datetime.time object as an ECMA-262 compliant
+    time string."""
     r = o.isoformat()
     if o.microsecond:
         r = r[:12]
@@ -20,4 +26,6 @@ def encode_time(o):
     return r
 
 def encode_decimal(o):
+    """ Encodes a Python decimal.Decimal object as an ECMA-262 compliant
+    decimal string."""
     return str(o)
